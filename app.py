17e82e23155b14916b1f0881159b6788faf49c22
vladislav20130105 <dukavladislav00@gmail.com>
2026-03-22 14:18:39 +0300

    добавил render конфигурацию

--- app.py
+++ app.py
@@ -290,4 +290,6 @@ for color in colors:
     return jsonify(examples)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
